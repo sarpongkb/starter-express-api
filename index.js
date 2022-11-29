8d0f6c2dc1fda9ac43e04cc9196a9f3806bff7f0
sarpongkb <sarpongkb@gmail.com>
2022-11-28 20:05:00 -0800

    fix typo

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 app.use(express.json());
 
 app.post('/saml/hook', (req, res) => {
-    console.log("Received request at 'saml/hook': ", request);
+    console.log("Received request at 'saml/hook': ", req);
     res.status(204).json();
 });
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.status(204).json();
+    res.status(204).json({data: "Yo!"});
 });
 
 app.listen(process.env.PORT || 3000);
