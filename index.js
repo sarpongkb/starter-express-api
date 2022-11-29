71d7cb61300f2ba37db25653defc243a28e2c9b0
sarpongkb <sarpongkb@gmail.com>
2022-11-28 20:44:10 -0800

    Return command to add sessionId to assertion

--- index.js
+++ index.js
@@ -5,7 +5,27 @@ app.use(express.json());
 
 app.post('/saml/hook', (req, res) => {
     console.log("Received request at 'saml/hook': ", req);
-    res.status(204).json();
+    
+    const sessionId = req.body?.data?.context?.session?.id;
+    
+    res.status(200).json({ 
+        "commands": [{
+            "type": "com.okta.assertion.patch",
+            "value": [{
+                "op": "add",
+                "path": "/claims/externalSessionId",
+                "value": {
+                    "attributes": {
+                        "NameFormat": "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified"
+                    },
+                    "attributeValues":[{
+                        "attributes": { "xsi:type": "xs:string" },
+                         "value": sessionId,
+                     }]
+                }
+            }]
+        }]
+    });
 });
 
 app.all('/', (req, res) => {
