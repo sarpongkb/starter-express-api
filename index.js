16db4e3182b1c66243519f03f5c0164e7cd9f020
sarpongkb <sarpongkb@gmail.com>
2022-11-28 19:56:19 -0800

    Add endpoint for saml hook

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/saml/hook', (req, res) => {
+    console.log("Received request at 'saml/hook': ", request);
+    res.status(204).json();
+});
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+});
+
+app.listen(process.env.PORT || 3000);
